c5c3d5cbcefeffe57c472eab6ac1b1bccaf11341
UCode <UCode@ucode-tpv-alpha.fios-router.home>
2018-07-15 16:03:34 -0700

    Did the view controller

--- calculator/Calculator.swift
+++ calculator/Calculator.swift
@@ -12,18 +12,22 @@ class Calculator {
     
     var input: Double
     var output: Double
+    var place: Int
     var operation: Operation
     
     init() {
         input = 0
         output = 0
         operation = .none
+        place = 0
         
     }
     
     func clear() {
         input = 0
         output = 0
+        place = 0
+        operation = .none
     }
     
     func sign() {
@@ -33,20 +37,51 @@ class Calculator {
     func percent() {
         input *= 0.01
     }
+    func decimal() {
+        if place == 0 {
+            place = -1
+        }
+    }
     
-    
-    func input(digit: Int) {
-        
-            
-        
-        
-        
+    func digit(_ entry: Int) {
+        if place == 0 {
+            input *= 10
+        }
+         input += Double(entry) * pow(10.0, Double(place))
+        if place < 0 {
+            place -= 1
+        }
+
         
     }
     
-    func operate(operation: Operation) {
-        // self.operation
-        // operation
+    func operate(_ operation: Operation) {
+        if self.operation == .none {
+            output = input
+        }
+        else{
+            equals()
+        }
+        input = 0
+        place = 0
+        self.operation = operation
     }
-        
+    func equals() {
+        switch operation {
+        case .addition:
+            output += input
+            break
+        case .subtraction:
+            output -= input
+            break
+        case .multiplication:
+            output *= input
+            break
+        case .division:
+            output /= input
+            break
+        default:
+            break
+        }
+    }
 }
--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -10,8 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var displayLabel: UILabel!
+    let calculator = Calculator()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        showInput()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +23,35 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func showInput() {
+        displayLabel.text = String(calculator.input)
+    }
+    func showOutput() {
+        displayLabel.text = String(calculator.output)
+    }
 
-
+    @IBAction func additionButtonPressed(_ sender: UIButton) {
+        calculator.operate(.addition)
+        showOutput()
+    }
+    
+    @IBAction func digitButtonPressed(_ sender: UIButton) {
+        calculator.digit(sender.tag)
+        showInput()
+    }
+    
+    @IBAction func clearButtonPressed (_ sender: UIButton){
+    calculator.clear()
+        showInput()
+}
+    @IBAction func signButtonPressed (_ sender: UIButton){
+        calculator.sign()
+        showInput()
+    }
+    @IBAction func percentButtonPressed (_ sender: UIButton){
+        calculator.percent()
+        showInput()
+    }
 }
 
